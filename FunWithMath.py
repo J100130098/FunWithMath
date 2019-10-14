7d3446217e0f89fb9e01aec7a5f57fcabf74c90d
100130098 <jjhill080103@gmail.com>
2019-10-14 08:57:57 -0500

    questions

--- FunWithMath.py
+++ FunWithMath.py
@@ -16,3 +16,23 @@ def archimedes(numSides):
 
 print(archimedes(8))
 print(archimedes(16))
+
+for sides in range(8, 100, 8):
+    print(sides, archimedes(sides))
+
+# experiment with loop above alongside the actual value of Pi. How many
+# sides does it take to make the two close?
+print(math.pi)
+# it takes about 100000000000000000 sides to get close to the value of pi. Yikes!
+print(archimedes(100000000000000000))
+
+# modify archimedes function to take the radius of the circle as a parameter
+# Can you get a better answer more quickly using a larger circle
+def archimedesParameter(numSides):
+    innerAngleB = 360.0 / numSides
+    halfAngleA = innerAngleB / 2
+    oneHalfSideS = math.sin(math.radians(halfAngleA))
+    sideS = oneHalfSideS * 2
+    polygonCircumference = numSides * sideS
+    pi = polygonCircumference / 2
+    return pi
